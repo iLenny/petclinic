92ac683f2e5f87064f0ee35d2e87153dce0e621b
Leibniz H. Berihuete <Leibniz.berihuete@gmail.com>
2020-01-29 21:06:37 -0600

    Add multiple modules

--- pet-clinic-web/src/main/java/XinbielPetClinicApplication.java
+++ pet-clinic-web/src/main/java/XinbielPetClinicApplication.java
@@ -1,5 +1,3 @@
-package com.xinbiel.petclinic.xinbielpetclinic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- pet-clinic-web/src/test/java/xinbielpetclinic/XinbielPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/xinbielpetclinic/XinbielPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xinbiel.petclinic.xinbielpetclinic;
+package xinbielpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
